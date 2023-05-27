c2b8dbb554062e64487cf508dd6a6264e34d1ee2
Vivek Alhat <vivekalhat14@outlook.com>
2023-05-28 02:06:56 +0530

    add login image asset

--- src/pages/Authentication.jsx
+++ src/pages/Authentication.jsx
@@ -4,9 +4,7 @@ const Authentication = () => {
   return (
     <div className="w-full h-screen grid grid-cols-1 sm:grid-cols-2">
       <img
-        src={
-          "https://images.unsplash.com/photo-1463320898484-cdee8141c787?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=870&q=80"
-        }
+        src={"/assets/plants.jpg"}
         alt={"Landing Image"}
         className="w-full h-screen object-cover hidden sm:block"
       />
