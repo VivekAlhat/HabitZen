26f1cfe6a1dd3afd8c4afbc9f174e671ab135986
Vivek Alhat <vivekalhat14@outlook.com>
2023-06-05 13:55:25 +0530

    add react-query

--- src/main.jsx
+++ src/main.jsx
@@ -4,24 +4,32 @@ import App from "./App.jsx";
 import "./index.css";
 
 import { BrowserRouter } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "react-query";
+import { ReactQueryDevtools } from "react-query/devtools";
+
 import ToastProvider from "./context/ToastContext";
 import UserProvider from "./context/UserContext.jsx";
 import ThemeProvider from "./context/ThemeContext.jsx";
 
 import * as DropdownPrimitive from "@radix-ui/react-dropdown-menu";
 
+const queryClient = new QueryClient();
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
-      <ThemeProvider>
-        <ToastProvider>
-          <UserProvider>
-            <DropdownPrimitive.Root>
-              <App />
-            </DropdownPrimitive.Root>
-          </UserProvider>
-        </ToastProvider>
-      </ThemeProvider>
+      <QueryClientProvider client={queryClient}>
+        <ThemeProvider>
+          <ToastProvider>
+            <UserProvider>
+              <DropdownPrimitive.Root>
+                <App />
+              </DropdownPrimitive.Root>
+            </UserProvider>
+          </ToastProvider>
+        </ThemeProvider>
+        <ReactQueryDevtools initialIsOpen={false} position="bottom-right" />
+      </QueryClientProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
