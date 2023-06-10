78b79a5bba314a9550dc54ef04c3c56074a772f2
Vivek Alhat <vivekalhat14@outlook.com>
2023-06-10 17:15:31 +0530

    create landing page

---
+++ src/pages/Landing.jsx
@@ -0,0 +1,12 @@
+import { Link } from "react-router-dom";
+
+const Landing = () => {
+  return (
+    <div>
+      Landing page
+      <Link to="/signin">Sign in</Link>
+    </div>
+  );
+};
+
+export default Landing;
